2d8f7e95b2652885a27842578353b027addf3996
Prasugupta21 <101421981+Prasugupta21@users.noreply.github.com>
2023-06-11 16:57:35 +0530

    add

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ passport.deserializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
   clientID:process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: "https://extinct-tuna-tutu.cyclic.app/auth/google/home",
+  callbackURL: "https://jade-charming-lemur.cyclic.app/auth/google/home",
   userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
 function(accessToken, refreshToken, profile, cb) {
